c2f847e5d567a52e1b6216da91bce9e94e153f04
JotaErrE <jruiz967@gmail.com>
2023-07-20 20:45:01 -0500

    fix: links mdx

--- src/utils/sortByDate.ts
+++ src/utils/sortByDate.ts
@@ -1,8 +1,7 @@
 import type { MDXInstance } from "astro";
 import type { IFrontmatter } from "interfaces/IFrontmatter";
 
-export const sortByDate = <T extends IFrontmatter>(data: MDXInstance<T>[]) => data.sort(
-  (a, b) =>
-    new Date(b.frontmatter.pubDate).valueOf() -
-    new Date(a.frontmatter.pubDate).valueOf()
+export const sortByDate = <T extends IFrontmatter>(data: MDXInstance<T>[]) => data.sort((a, b) =>
+  new Date(b.frontmatter.pubDate).valueOf() -
+  new Date(a.frontmatter.pubDate).valueOf()
 );
