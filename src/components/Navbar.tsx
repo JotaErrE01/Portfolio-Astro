9106c19c8f009d88f60d557c2b94c971cca07f89
JotaErrE01 <jruiz967@gmail.com>
2024-05-18 22:50:19 -0500

    style: jump in styles solve

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,29 +49,32 @@ export const Navbar = ({ data }: Props) => {
   };
 
   return (
-    <header className="navbar bg-base-100 my-5 border rounded-md border-primary shadow-sm shadow-secondary mx-auto">
+    <header className="navbar bg-base-100 border rounded-md border-primary shadow-sm shadow-secondary mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost btn-circle">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
           </label>
-          <ul id='nav' tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li
-              className='list'
-            ><a
-              href='/'
-            >Inicio</a></li>
-            <li
-              className='list'
-            ><a
-              href='/posts/'
-            >Posts</a></li>
-            <li
-              className='list'
-            ><a
-              href='/resources/'
-            >Recursos</a></li>
-          </ul>
+
+          <nav>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <li
+                className='list'
+              ><a
+                href='/'
+              >Inicio</a></li>
+              <li
+                className='list'
+              ><a
+                href='/posts/'
+              >Posts</a></li>
+              <li
+                className='list'
+              ><a
+                href='/resources/'
+              >Recursos</a></li>
+            </ul>
+          </nav>
         </div>
       </div>
       <div className="navbar-center">
