9a3244935e6539ea6e6d71a80780867393190503
JotaErrE <jruiz967@gmail.com>
2024-05-30 21:11:42 -0500

    feat: time tag for google seo

--- src/interfaces/IFrontmatter.ts
+++ src/interfaces/IFrontmatter.ts
@@ -1,9 +1,18 @@
 
+// export interface IFrontmatter {
+//   slug: string;
+//   title: string;
+//   date: string;
+//   pubDate: string;
+//   images: string;
+//   shortDescription: string;
+// }
 export interface IFrontmatter {
-  slug: string;
   title: string;
-  date: string;
-  pubDate: string;
-  images: string;
   shortDescription: string;
+  images: string[];
+  slug: string;
+  date: string;
+  keywords: string[];
+  pubDate: string;
 }
